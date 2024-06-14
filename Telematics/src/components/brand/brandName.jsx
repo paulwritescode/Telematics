c7e0c2cf0a96ce34b693570583b2855c915e18f7
luna <paulkingdev7@gmail.com>
2024-06-14 12:46:58 +0300

    @manqiqode #1: Telematics starting now

---
+++ Telematics/src/components/brand/brandName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function brandName() {
+  return <div>Fleet</div>;
+}
+
+export default brandName;
---
+++ Telematics/src/components/dummyInfo/dummyUser.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const dummyUser = () => {
+  return <div>dummyUser Data </div>;
+};
